8b181b19299ac9c22b9d84d9e3ef1d43b9bdf811
Soumya Sharma <soumyas1@uci.edu>
2020-05-10 23:51:11 -0700

    Changed employee table sql script

--- employeeTable.sql
+++ employeeTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `employees` (
-  `username` varchar(5) NOT NULL,
-  `password` varchar(10) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
   `role` varchar(45) NOT NULL,
   PRIMARY KEY (`username`)
 );
