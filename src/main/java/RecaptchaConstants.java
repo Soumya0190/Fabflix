ca51af80147078bcf217aa0f2e8c56e5240e36b3
Soumya Sharma <soumyas1@uci.edu>
2020-05-11 00:24:50 -0700

    Recaptcha

--- src/main/java/RecaptchaConstants.java
+++ src/main/java/RecaptchaConstants.java
@@ -2,7 +2,8 @@ package main.java;
 
 public class RecaptchaConstants {
 
-    public static final String SECRET_KEY ="6Ld6e_EUAAAAANcl057DucIMgCsiZmfnvPc2lxmI";
+    //public static final String SECRET_KEY ="6Ld6e_EUAAAAANcl057DucIMgCsiZmfnvPc2lxmI";
+    public static final String SECRET_KEY ="6LcZjvUUAAAAABrkWtRPR1DUTtjdjMKanyi-ZRxx";
 
 }
 
