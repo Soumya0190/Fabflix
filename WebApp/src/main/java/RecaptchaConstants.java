e886aea641a59d76a9806343a8f4f9175cd0fbdb
Soumya0190 <ssoumya0119@gmail.com>
2020-05-25 02:25:49 -0700

    recaptcha

--- WebApp/src/main/java/RecaptchaConstants.java
+++ WebApp/src/main/java/RecaptchaConstants.java
@@ -3,7 +3,7 @@ package main.java;
 public class RecaptchaConstants {
 
     //public static final String SECRET_KEY ="6Ld6e_EUAAAAANcl057DucIMgCsiZmfnvPc2lxmI"; //LOCAL
-    public static final String SECRET_KEY ="6LcZjvUUAAAAABrkWtRPR1DUTtjdjMKanyi-ZRxx"; //AWS
+    public static final String SECRET_KEY ="6Lcn6fsUAAAAAMH8X9UzeCxUoZBn4WufXf_yqM2m"; //AWS
 
 }
 
